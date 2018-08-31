a6f42e0220fb2341e66fe448a7ccf47b06dc3b97
fengshuo <253598463@qq.com>
2018-08-31 18:31:30 +0800

    完成首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,52 +3,88 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+    data: {
+        swiperCurrent: 0,
+        images: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+        categories: [],
+        activeCategoryId: 0,
+        scrollTop: "0",
+    },
+    swiperchange: function(e) {
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+            swiperCurrent: e.detail.current
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    },
+    tabClick: function(e) {
+        this.setData({
+            activeCategoryId: e.currentTarget.id
+        })
+    },
+    scroll: function (e) {
+        console.log(e);
+        var that = this, scrollTop = that.data.scrollTop;
+        that.setData({
+            scrollTop: e.detail.scrollTop
+        })
+        console.log('scrollTop:'+scrollTop)
+    },
+    onLoad: function() {
+        var that = this;
+
+        wx.request({
+            url: 'https://api.it120.cc/' + app.globalData.subDomain + '/shop/goods/category/all',
+            success: function(res) {
+                var categories = [];
+                for (var i = 0; i < res.data.data.length; i++) {
+                    categories.push(res.data.data[i]);
+                }
+                categories.push({ id: 1, name: '123' })
+                categories.push({ id: 2, name: '123' })
+                categories.push({ id: 3, name: '123' })
+                categories.push({ id: 4, name: '123' })
+                categories.push({ id: 5, name: '123' })
+                categories.push({ id: 6, name: '123' })
+                console.log(categories)
+                that.setData({
+                    categories: categories,
+                    activeCategoryId: res.data.data[0].id
+                });
+                that.getGoodsList(that.data.activeCategoryId);
+            }
+        })
+    },
+    getGoodsList: function(categoryId) {
+        var that = this;
+        var initimg = ['../../images/goods-index/baby.jpg',
+        '../../images/goods-index/puxiang.jpg',
+        '../../images/goods-index/pidai.jpg',
+        '../../images/goods-index/shangpu.jpg',
+        '../../images/goods-index/yifu.jpg']
+        wx.request({
+            url: 'https://api.it120.cc/' + app.globalData.subDomain + '/shop/goods/list',
+            data: {
+                categoryId: categoryId
+            },
+            success: function(res) {
+                that.setData({
+                    goods: [],
+                    loadingMoreHidden: true
+                });
+                var goods = [];
+                if (res.data.code != 0 || res.data.data.length == 0) {
+                    that.setData({
+                        loadingMoreHidden: false,
+                    });
+                    return;
+                }
+                for (var i = 0; i < res.data.data.length; i++) {
+                    res.data.data[i].pic = initimg[i];
+                    goods.push(res.data.data[i]);
+                }
+                that.setData({
+                    goods: goods,
+                });
+            }
+        })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
